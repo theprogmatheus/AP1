894a7b65ab69c9021f97c70b54826498262ff8c2
theprogmatheus <theprog.matheus@gmail.com>
2025-05-03 10:28:43 -0300

    Resolução do exercício 3 do lab5

---
+++ LAB5/exc3lab5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Digite um número decimal:");
+    scanf("%d", &num);
+    printf("Numero em Hexa: %x\n", num);
+    printf("Numero em Octa: %o\n", num);
+}
