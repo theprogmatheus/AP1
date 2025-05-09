fd9e2e0949ad206040dd4f3f2976cf04a5ec2a61
theprogmatheus <theprog.matheus@gmail.com>
2025-05-09 10:23:07 -0300

    solve exc7 lab6

--- LAB6/exc7.c
+++ LAB6/exc7.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
-void main(){
-    
+void main()
+{
+
+    int fat, out;
+    printf("Insira um fatorial: ");
+    scanf("%d", &fat);
+
+    if (fat == 1 || fat == 0)
+        out = 1;
+    else
+    {
+        while (fat > 0)
+        {
+            if (out == 0)
+                out = 1;
+
+            out *= fat;
+            fat--;
+        }
+    }
+
+    printf("Valor da fatorial: %d\n", out);
 }
