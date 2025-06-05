365c6009ec28fbc2a7b24986fb3a190e2b3204c1
theprogmatheus <theprogmatheus@gmail.com>
2025-06-05 09:49:31 -0300

    solve exc4 lib9

--- LAB9/exc4.c
+++ LAB9/exc4.c
@@ -2,5 +2,20 @@
 
 void main()
 {
-    
+    int a[10], count = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Insira o valor da posição %d: ", i);
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < (sizeof(a) / sizeof(a[0])); i++)
+    {
+        int element = a[i];
+        if (element % 2 == 0)
+            count++;
+    }
+
+
+    printf("Existem %d valor(es) par(es) no primeiro vetor.\n");
 }
